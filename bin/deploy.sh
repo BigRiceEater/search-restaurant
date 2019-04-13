c6e06eb2631bfb0885f88d603ca1926d7f059e00
big.rice.eater <34292181+BigRiceEater@users.noreply.github.com>
2019-04-14 01:14:13 +0800

    Fix deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-git checkout master
+git checkout gh-pages
+git merge master
 npm run build
 git add ./dist
 git commit 
-git checkout gh-pages
-git merge master
 git subtree push --prefix dist origin gh-pages
 git checkout master
